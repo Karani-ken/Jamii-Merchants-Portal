5eba0d8850538412e54865ca9dc0b2ec1de92788
karani-ken <academice79@gmail.com>
2024-01-24 15:27:01 +0300

    Created a table

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import AdminDasboard from './components/AdminDasboard';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
+      <Sidebar/>
         <AdminDasboard/>
     </div>
   );
--- client/src/components/AdminDasboard.jsx
+++ client/src/components/AdminDasboard.jsx
@@ -3,8 +3,54 @@ import Sidebar from './Sidebar'
 
 function AdminDasboard() {
   return (
-    <div className='' style={{height:'100vh'}}>
-            <Sidebar/>
+    <div className='admin-panel'>
+      <h1 className='fw-bolder'>Welcome Admin</h1>
+      <div className="card-container">
+        <div className="stats-card">
+          <h2>9</h2>
+          <p>Total Users</p>
+        </div>
+        <div className="stats-card">
+          <h2>20</h2>
+          <p>Allocated serials</p>
+        </div>
+      </div>
+      <table className='table table-striped-columns table-hover'>
+        <thead>
+          <tr>
+            <th scope='col'>No.</th>
+            <th scope='col'> Name</th>
+            <th scope='col'> Email</th>
+            <th scope='col'>Phone</th>
+            <th scope='col'>Serials</th>
+          </tr>
+        </thead>
+        <tbody className='table-group-divider'>
+          <tr>
+            <th scope='row'>1</th>
+            <td>John Doe</td>
+            <td>johndoe@exampe.com</td>
+            <td>0712345678</td>
+            <td>6</td>
+          </tr>
+          <tr>
+            <th scope='row'>2</th>
+            <td>John Doe</td>
+            <td>johndoe@exampe.com</td>
+            <td>0712345678</td>
+            <td>10</td>
+          </tr>
+          <tr>
+            <th scope='row'>3</th>
+            <td>John Doe</td>
+            <td>johndoe@exampe.com</td>
+            <td>0712345678</td>
+            <td>15</td>
+          </tr>
+        </tbody>
+      </table>
+
+
     </div>
   )
 }
