7b2f30673409031f91f5064139920606c0324039
karani-ken <academice79@gmail.com>
2024-03-09 09:09:15 +0300

    made some chnages

--- Backend/index.js
+++ Backend/index.js
@@ -3,15 +3,18 @@ const express = require('express')
 const dbHandler = require('./Database/dbHandler')
 const bodyParser = require('body-parser');
 const authRoutes = require('./Routes/auth.routes')
+const cors = require('cors')
 const customerRoutes = require('./Routes/customer.routes')
 const path = require('path')
 const app = express();
 const port = process.env.PORT;
 app.use(bodyParser.json())
+app.use(cors())
 //connect to a database
 dbHandler.pool.getConnection((err, connection)=>{
     if(err) throw err;
     console.log("MySQL connected successfully");
+    `<h1> mysql connected successfully </h1>`
     dbHandler.initializeDatabase()
     .then(()=>{
        connection.release()
@@ -21,13 +24,8 @@ dbHandler.pool.getConnection((err, connection)=>{
         throw err;    
     })
 })
-app.use(express.static(path.join(__dirname, '../client/build')));
+
 app.use('/auth', authRoutes);
 app.use('/customer', customerRoutes);
-app.get('*', (req,res)=>{
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
-} )
 
-app.listen(port,()=>{
-    console.log(`App started on http://localhost:${port}`);
-})      
+app.listen();
